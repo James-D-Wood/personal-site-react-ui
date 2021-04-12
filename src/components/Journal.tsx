9a65344995a2479d331e2aaf533b5b2e253b599b
James Wood <woodjames275@gmail.com>
2021-04-11 19:10:26 -0700

    feat: connect Journal component to backend

--- src/components/Journal.tsx
+++ src/components/Journal.tsx
@@ -1,65 +1,50 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { v4 as uuidv4 } from 'uuid';
-import JournalEntryData from '../types/JournalEntryData';
+import { JournalEntryData, JournalEntriesDTO } from '../types/journalEntries';
+import Loader from './Loader';
 
 export default function Journal() {
-  // Needs to render a list of entry previews
-  /*
-    Entry preview:
-      - Date
-      - Summary
-      - Tags
-
-    So where should I fetch the entries?
-    Let's try lifting the state first.
-    I'll use a state hook to hold the entries
-    and an effect hook to load entries.
-  */
-
   const urlDate = (timestamp: number) => {
     const date = new Date(timestamp);
     return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
   };
 
   const urlTitle = (title: string) => title.split(' ').join('-').toLowerCase();
-  // Data or component?
-  // Try data first
-  const dummyData = [
-    {
-      id: uuidv4(),
-      title: 'Entry 1',
-      timestamp: Date.now(),
-      description: 'Here are some things I learned today.',
-    },
-  ];
 
-  const [entries] = useState<JournalEntryData[]>(dummyData);
+  const [isLoading, setIsLoading] = useState(true);
+  const [entries, setEntries] = useState<JournalEntryData[]>([]);
   const [entryPreviews, setEntryPreviews] = useState<React.ReactNode[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/journal/entries')
+      .then((response) => response.json())
+      .then((data: JournalEntriesDTO) => {
+        setEntries(data.data);
+        setIsLoading(false);
+      });
+  }, []);
 
   // Does this effect just double the render each time entries is updated?
   useEffect(() => {
-    console.log('Updating entry previews.', entries);
     setEntryPreviews(entries.map((entry) => (
       <li key={entry.title}>
         <p>{entry.title}</p>
         <p>{entry.timestamp}</p>
-        <Link to={
-          {
-            pathname: `/journal/entries/${urlDate(entry.timestamp)}/${urlTitle(entry.title)}`,
-          }
-        }
-        >
+        <Link to={`/journal/entries/${urlDate(entry.timestamp)}/${urlTitle(entry.title)}`}>
           Show Entry
         </Link>
       </li>
     )));
   }, [entries]);
 
-  return (
-    <div>
-      <h1>Welcome to the Dev Journal</h1>
-      <ul>{entryPreviews}</ul>
-    </div>
-  );
+  return isLoading
+    ? (
+      <Loader />
+    )
+    : (
+      <div>
+        <h1>Welcome to the Dev Journal</h1>
+        <ul>{entryPreviews}</ul>
+      </div>
+    );
 }
--- src/components/JournalEntry.tsx
+++ src/components/JournalEntry.tsx
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
+import marked from 'marked';
+import DOMPurify from 'dompurify';
+import { JournalEntryData } from '../types/journalEntries';
 
 interface JournalEntryParams {
   date: string,
+  title: string,
 }
 
 export default function JournalEntry() {
-  const { date } = useParams<JournalEntryParams>();
+  const { date, title } = useParams<JournalEntryParams>();
+  const [journalEntryData, setJournalEntryData] = useState<JournalEntryData | null>(null);
+
+  const dummyData:JournalEntryData = {
+    id: uuidv4(),
+    title,
+    timestamp: Date.now(),
+    description: 'A description',
+    mdBody: '## Getting Started\nWhen getting started with markdown consider this.',
+    htmlBody: marked('## Getting Started\nWhen getting started with markdown consider this.'), // TODO: sanitize
+  };
+
+  useEffect(() => {
+    setJournalEntryData(dummyData);
+  }, []);
 
   return (
-    <h1>{ date }</h1>
+    <>
+      <h1>{ date }</h1>
+      { journalEntryData
+        && (
+          <div dangerouslySetInnerHTML={
+            { __html: DOMPurify.sanitize(journalEntryData.htmlBody) }
+          }
+          />
+        )}
+    </>
   );
 }
