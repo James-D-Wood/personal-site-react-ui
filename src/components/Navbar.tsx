9cdb1acd7aa91e079b294a0ff68617d39fad7468
James Wood <woodjames275@gmail.com>
2021-06-22 21:31:30 -0700

    feat: make navbar responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import MenuIcon from '../icons/MenuIcon';
 import { MenuItem } from '../utils/MenuItems';
 import MobileMenu from './MobileMenu';
@@ -13,11 +14,27 @@ export default function Navbar(props: NavbarProps): JSX.Element {
 
   const [isShowingMobileMenu, setIsShowingMobileMenu] = useState(false);
 
+  const menuLinks = menuItems.map((menuItem: MenuItem) => (
+    <li key={menuItem.displayText.split(' ').join()}>
+      <Link
+        className="text-white hover:text-purple-light font-sansBold text-lg flex flex-row items-center"
+        to={menuItem.path}
+      >
+        {/* <div className="text-purple-light mx-4">{menuItem.icon}</div> */}
+        {menuItem.displayText}
+      </Link>
+    </li>
+  ));
+
   return (
     <div className="fixed w-full pointer-events-none">
       <div className="px-8 py-4 bg-purple-dark items-center shadow md:px-12 flex justify-between bg-green-500 pointer-events-auto">
         {logo}
+        <ul className="hidden lg:flex lg:flex-row space-x-6">
+          {menuLinks}
+        </ul>
         <button
+          className="lg:hidden"
           type="button"
           onClick={() => { setIsShowingMobileMenu(!isShowingMobileMenu); }}
         >
